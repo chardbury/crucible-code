c9b9003b04c2ac6a7ff34baa22e76f60b730ee21
Richard Thomas <ri@chrd.uk>
2019-05-05 21:19:24 +0100

    Modify new server script for our infrastructure

--- scripts/linodeGameServerRootSetup.sh
+++ scripts/linodeGameServerRootSetup.sh
@@ -17,7 +17,7 @@ chmod 744 .ssh
 
 cd .ssh
 
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAsiD85AwaNxcUzNzHPapVaGVIQCTUfdKT2tyd26MWqEds2UHLZund+S930BWz7guu3/mzuTomJnPxZPSyb+62ZiuAR0YaGZwYwMrFkrbPsXf6//MZDBvdMMcqPyqLj3Iny2ZZ9LTnSIs0hqQ3SksvP/qqHthS1YQWMwZlRxs6MmZdEuy4qZZgpnexf6uaWTEcxEO2Nij8LdEN8+jJZLHVkXSSD9c/ssTmdXss3/sVSZHYR+28HeqUahRsO0Rz6mR3FwB+ZslZlWiMFTzjkH5IA/XOiNK5Ezf1+EsQOn2OVfaCMlfJQ6YGp1kgFI01j2ZWHnqHaacvVc+C+9fAmIscZw==" > authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDXCsgHA0xvxll5C17p9p7aBQ0lsRCzpG1UhiKviG4ikAAHo0lHd8ywhCumycYJxNHkkWOxNrVkDEb8Ge4lVaJCpGzqkvDth/qhm7MUZZCQIITaYXW89ODGpHMCMBnq6y7FpEBkYQR9Cr4wB8GygUjuE1jhPnL6GRLooV8tLZ0DX4T5rFTfNHfLpkJ+BpoPwDW/rf2RcGO/e1vCuoDQQY/85tCyRs3cRv6yQkRPKyzOESJjhqri6j34e7fzPuVJzKqQlbiuvYoqLweoUbqEd0CwtYIhy9yXCeZSb1ZrIFQ+pNbaoapOgQqe9+g8T0WBOTx1Punwr6AGQWDxM6o5Qse0EeTLbUubOLfddavZueL/yUqQt9gpB64VRBeUZFf7ZmRS4VFi4YbSNvoldurnDu7E0VllXCsw8uCYNHBgyqgqDAmWho887uJ+5dxKsz6ZVH98aLDpUjCJqN5kqBF09F2SeO2AUtvvEabq86frwUhs5pAffpjY6EcyMO+cTK8/i5J2SYCQieFAOH/S1c7kjDvZOAaxRcPFi9r3sT/CO39p7fE5qwgPC7eFRgJAV59WS1bBbd31/QFFSwUgeHN9DAKLtBcWUWCZqCRgZxVYfdosSW/RXhiw/lbIUw+B1t3PfBRaG9WvvJA0HvE4UyWPpYY+YizxrTa/gcqt2aPT479F9w== richard@crucible" > authorized_keys
 
 chmod 644 authorized_keys
 
@@ -45,8 +45,7 @@ echo "Opening port 8005 for game server"
 echo ""
 echo ""
 
-ufw allow from 72.14.184.149 to any port 22
-ufw allow from 173.230.147.48 to any port 22
+ufw allow from 176.58.100.188 to any port 22
 ufw allow 8005
 ufw --force enable
 
@@ -67,19 +66,19 @@ echo "kernel.core_pattern=core.%e.%p.%t" >> /etc/sysctl.conf
 
 echo ""
 echo ""
-echo "Setting up new user account jcr13 without a password..."
+echo "Setting up new user account richard without a password..."
 echo ""
 echo ""
 
-useradd -m -s /bin/bash jcr13
+useradd -m -s /bin/bash richard
 
 
 dataName="OneLifeData7"
 
 
-su jcr13<<EOSU
+su richard<<EOSU
 
-cd /home/jcr13
+cd /home/richard
 
 echo "ulimit -c unlimited >/dev/null 2>&1" >> ~/.bash_profile
 
@@ -92,21 +91,21 @@ cd checkout
 
 echo "Using data repository $dataName"
 
-git clone https://github.com/jasonrohrer/OneLife.git
-git clone https://github.com/jasonrohrer/$dataName.git
-git clone https://github.com/jasonrohrer/minorGems.git
+git clone https://github.com/chardbury/crucible-code.git OneLife
+git clone https://github.com/chardbury/crucible-data.git $dataName
+git clone https://github.com/chardbury/crucible-gems.git minorGems
 
 
 cd $dataName
 
-lastTaggedDataVersion=\`git for-each-ref --sort=-creatordate --format '%(refname:short)' --count=1 refs/tags/OneLife_v* | sed -e 's/OneLife_v//'\`
+lastTaggedDataVersion=\`git for-each-ref --sort=-creatordate --format '%(refname:short)' --count=1 refs/tags/Crucible_v* | sed -e 's/Crucible_v//'\`
 
 
 echo "" 
 echo "Most recent Data git version is:  \$lastTaggedDataVersion"
 echo ""
 
-git checkout -q OneLife_v\$lastTaggedDataVersion
+git checkout -q Crucible_v\$lastTaggedDataVersion
 
 
 
@@ -120,7 +119,7 @@ ln -s ../../$dataName/categories .
 ln -s ../../$dataName/tutorialMaps .
 ln -s ../../$dataName/dataVersionNumber.txt .
 
-git for-each-ref --sort=-creatordate --format '%(refname:short)' --count=1 refs/tags/OneLife_v* | sed -e 's/OneLife_v//' > serverCodeVersionNumber.txt
+git for-each-ref --sort=-creatordate --format '%(refname:short)' --count=1 refs/tags/Crucible_v* | sed -e 's/Crucible_v//' > serverCodeVersionNumber.txt
 
 
 ./configure 1
@@ -131,17 +130,17 @@ bash -l ./runHeadlessServerLinux.sh
 
 echo -n "1" > ~/keepServerRunning.txt
 
-crontab /home/jcr13/checkout/OneLife/scripts/remoteServerCrontabSource
+crontab /home/richard/checkout/OneLife/scripts/remoteServerCrontabSource
 
 
-cd /home/jcr13
+cd /home/richard
 mkdir .ssh
 
 chmod 744 .ssh
 
 cd .ssh
 
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAsiD85AwaNxcUzNzHPapVaGVIQCTUfdKT2tyd26MWqEds2UHLZund+S930BWz7guu3/mzuTomJnPxZPSyb+62ZiuAR0YaGZwYwMrFkrbPsXf6//MZDBvdMMcqPyqLj3Iny2ZZ9LTnSIs0hqQ3SksvP/qqHthS1YQWMwZlRxs6MmZdEuy4qZZgpnexf6uaWTEcxEO2Nij8LdEN8+jJZLHVkXSSD9c/ssTmdXss3/sVSZHYR+28HeqUahRsO0Rz6mR3FwB+ZslZlWiMFTzjkH5IA/XOiNK5Ezf1+EsQOn2OVfaCMlfJQ6YGp1kgFI01j2ZWHnqHaacvVc+C+9fAmIscZw==" > authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDXCsgHA0xvxll5C17p9p7aBQ0lsRCzpG1UhiKviG4ikAAHo0lHd8ywhCumycYJxNHkkWOxNrVkDEb8Ge4lVaJCpGzqkvDth/qhm7MUZZCQIITaYXW89ODGpHMCMBnq6y7FpEBkYQR9Cr4wB8GygUjuE1jhPnL6GRLooV8tLZ0DX4T5rFTfNHfLpkJ+BpoPwDW/rf2RcGO/e1vCuoDQQY/85tCyRs3cRv6yQkRPKyzOESJjhqri6j34e7fzPuVJzKqQlbiuvYoqLweoUbqEd0CwtYIhy9yXCeZSb1ZrIFQ+pNbaoapOgQqe9+g8T0WBOTx1Punwr6AGQWDxM6o5Qse0EeTLbUubOLfddavZueL/yUqQt9gpB64VRBeUZFf7ZmRS4VFi4YbSNvoldurnDu7E0VllXCsw8uCYNHBgyqgqDAmWho887uJ+5dxKsz6ZVH98aLDpUjCJqN5kqBF09F2SeO2AUtvvEabq86frwUhs5pAffpjY6EcyMO+cTK8/i5J2SYCQieFAOH/S1c7kjDvZOAaxRcPFi9r3sT/CO39p7fE5qwgPC7eFRgJAV59WS1bBbd31/QFFSwUgeHN9DAKLtBcWUWCZqCRgZxVYfdosSW/RXhiw/lbIUw+B1t3PfBRaG9WvvJA0HvE4UyWPpYY+YizxrTa/gcqt2aPT479F9w== richard@crucible" > authorized_keys
 
 chmod 644 authorized_keys
 
